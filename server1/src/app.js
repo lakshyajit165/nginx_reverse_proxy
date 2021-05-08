8f7f9e206e688393fed85b7e52a978e2ea124f56
Lakshyajit <lakshyajit165@gmail.com>
2021-05-08 12:07:40 +0530

    added rewrite rules

--- server1/src/app.js
+++ server1/src/app.js
@@ -4,4 +4,12 @@ app.get('/', (req, res) => {
   return res.status(200).send({ "message": "Response from server1!" });
 });
 
+app.get('/app', (req, res) => {
+  return res.status(200).send({ "message": "App Response from server1!" });
+});
+
+app.get('/app/v1', (req, res) => {
+  return res.status(200).send({ "message": "App v1 Response from server1!" });
+});
+
 app.listen(5001, () => console.log('Server is up and running'));
--- server2/src/app.js
+++ server2/src/app.js
@@ -4,4 +4,12 @@ app.get('/', (req, res) => {
   return res.status(200).send({ "message": "Response from server2!" });
 });
 
+app.get('/app', (req, res) => {
+  return res.status(200).send({ "message": "App Response from server2!" });
+});
+
+app.get('/app/v1', (req, res) => {
+  return res.status(200).send({ "message": "App v1 Response from server2!" });
+});
+
 app.listen(5002, () => console.log('Server is up and running'));
