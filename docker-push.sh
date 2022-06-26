e720ac365e73d18a29540c60e098d1422e1094d6
Lakshyajit Laxmikant <lakshyajit165@gmail.com>
2022-06-26 11:37:01 +0530

    modified image pull policy

--- docker-push.sh
+++ docker-push.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 cat ./docker_hub_pwd.txt | docker login --username lakshyajit165 --password-stdin
-docker build -t k8s_demo_server1 ./server1
+
+# for platform specific building of docker containers on M1 processors
+docker build --platform linux/amd64 -t k8s_demo_server1 ./server1
 docker tag k8s_demo_server1 lakshyajit165/k8s_demo_server1
 docker push lakshyajit165/k8s_demo_server1
 
-docker build -t k8s_demo_server2 ./server2
+docker build --platform linux/amd64 -t k8s_demo_server2 ./server2
 docker tag k8s_demo_server2 lakshyajit165/k8s_demo_server2
 docker push lakshyajit165/k8s_demo_server2
 
-docker build -t k8s_demo_client ./client
+docker build --platform linux/amd64 -t k8s_demo_client ./client
 docker tag k8s_demo_client lakshyajit165/k8s_demo_client
 docker push lakshyajit165/k8s_demo_client
